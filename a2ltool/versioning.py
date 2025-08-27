1c78e0d8c19c40f62687e312320a4076eb4c4c28
Saikiran-Bolla1 <saikiranbolla1@gmail.com>
2025-08-27 20:12:50 +0530

    Delete a2ltool directory

--- a2ltool/versioning.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-from .a2l import A2LDocument
-
-
-def change_a2l_version(doc: A2LDocument, target_version: str) -> A2LDocument:
-    return doc.annotate_version(target_version)
